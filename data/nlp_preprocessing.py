a1e4d602cc6e314bc0b2f05a1cd3335388aab604
Bilal Rukundi <bilalrukundi1658@gmail.com>
2026-08-09 02:53:12 +0530

    Update NLP preprocessing (NLP 2/4)

--- data/nlp_preprocessing.py
+++ data/nlp_preprocessing.py
@@ -6,35 +6,56 @@ from typing import List
 class Solution:
     def get_dataset(self, positive: List[str], negative: List[str]) -> TensorType[float]:
         # 1. Build vocabulary: collect all unique words, sort them, assign integer IDs starting at 1
-        combined_list = positive + negative
-
-        splitted_list = [word for string in combined_list for word in string.split()]
-
-        unique_sorted_list = sorted(set(splitted_list))
-
         # 2. Encode each sentence by replacing words with their IDs
-        output = None
-        word_map = {}
+        # 3. Combine positive + negative into one list of tensors
+        # 4. Pad shorter sequences with 0s using nn.utils.rnn.pad_sequence(tensors, batch_first=True)
 
-        for ID, word in enumerate(unique_sorted_list, start=1):
-            word_map[word] = ID
+        # Building the vocab: Splitting and then collecting all the unique words + sortinga and then assiging ids starting at 1
 
+        def get_vocab(l_1: List[str], l_2: List[str]) -> List[str]:
+            merged_list = l_1 + l_2
+
+            words_list = [word for string in merged_list for word in string.split()]
+
+            unique_words_list = sorted(set(words_list))
+
+            return unique_words_list
+
+        word_list = get_vocab(positive, negative)
+
+        def build_word_map(word_list: List[str]) -> dict:
+            word_map = {}
+            for token_id, word in enumerate(word_list, start=1):
+                word_map[word] = token_id
+            return word_map 
+
+        word_map = build_word_map(word_list)
+        
         def encode_sentence(sentences):
             tensors = []
-            for sentence in sentences:
+            
+            for sentence in sentences: 
+
+                tensor = []
 
                 encode_id = [word_map[word] for word in sentence.split()]
 
-                tensor_sm = torch.tensor(encode_id, dtype=torch.float)
+                encoded_id_tensor = torch.tensor(encode_id, dtype=torch.float) # Converting the list into a pytorch tensor
 
-                tensors.append(tensor_sm)
+                tensors.append(encoded_id_tensor)
+
             return tensors
 
-        output = encode_sentence(positive) + encode_sentence(negative)
+        
+
+        encoded_sentence = encode_sentence(positive) + encode_sentence(negative)
 
 
-        padded_dataset = nn.utils.rnn.pad_sequence(
-            output, batch_first=True, padding_value=0.0
+        output = nn.utils.rnn.pad_sequence(
+            encoded_sentence,
+            batch_first=True,
+            padding_value=0.0
         )
 
-        return padded_dataset
+
+        return output
